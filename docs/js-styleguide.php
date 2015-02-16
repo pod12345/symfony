92e57c9439d66143e8e6bfb88cbc13aefa022f5b
Mark Tertishniy <mtertishniy@gmail.com>
2015-02-16 10:50:38 +1000

    Добавляю стайлгайды для js и php

---
+++ docs/js-styleguide.php
@@ -0,0 +1,33 @@
+# JS Style Guide
+
+This is a style guide for JavaScript, which should be used when contributing this project.
+
+You should follow the standards defined in the [airbnb-styleguide][] with some exceptions.
+
+## Whitespace
+
+  - Use soft tabs set to 3 spaces.
+
+    ```javascript
+    // bad
+    function() {
+    ∙∙∙∙var name;
+    }
+
+    // bad
+    function() {
+    ∙∙var name;
+    }
+
+    // bad
+    function() {
+    ∙var name;
+    }
+
+    // good
+    function() {
+    ∙∙∙var name;
+    }
+    ```
+
+[airbnb-styleguide]: https://github.com/airbnb/javascript#airbnb-javascript-style-guide-
